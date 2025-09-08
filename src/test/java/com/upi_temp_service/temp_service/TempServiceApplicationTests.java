fa9266731fb651fa7efa52b2169a01af512cc840
guunadharr3i <guunadharr.neelikottaam@3i-infotech.com>
2025-09-08 16:55:58 +0530

    commented tempServiceApplicationTest.java file

--- src/test/java/com/upi_temp_service/temp_service/TempServiceApplicationTests.java
+++ src/test/java/com/upi_temp_service/temp_service/TempServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package com.upi_temp_service.temp_service;
+// package com.upi_temp_service.temp_service;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class TempServiceApplicationTests {
+// @SpringBootTest
+// class TempServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// @Test
+// void contextLoads() {
+// }
 
-}
+// }
